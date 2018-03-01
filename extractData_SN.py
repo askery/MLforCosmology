ec0adbbb9191f4070981619d4f287a19fa5d8bca
askery <askery@gmail.com>
2018-03-01 19:49:06 -0300

    main alteration - reading all the files in the folder

--- extractData_SN.py
+++ extractData_SN.py
@@ -16,51 +16,48 @@ import os
 path = '/home/data/research/IIP/ML/colab/luciano/rawdata/SIMGEN_PUBLIC_DES'
 files = [f for f in os.listdir(path) if f.endswith('.DAT')]
 
-
-file = '/home/data/research/IIP/ML/colab/luciano/rawdata/SIMGEN_PUBLIC_DES/DES_SN000017.DAT'
-
-with open(file, 'r') as infile:
-    line = infile.readlines()
-    
-data = []    
-for i in line:
-    if 'Type' in i:
-        sntype = i
-    if 'VARLIST' in i:
-        header = i
-    if 'OBS:' in i:
-        data.append(i)
+for file in files:
+    with open(path + '/' + file, 'r') as infile:
+        line = infile.readlines()
         
-for i in data:
-    if 'NOBS' in i:
-        data.remove(i)
+    data = []    
+    for i in line:
+        if 'Type' in i:
+            sntype = i
+        if 'VARLIST' in i:
+            header = i
+        if 'OBS:' in i:
+            data.append(i)
+            
+    for i in data:
+        if 'NOBS' in i:
+            data.remove(i)
+    
+    typeSN = sntype[sntype.find("=")+1:sntype.find(",")]
+    typeSN = typeSN.strip()
+    
+    features = header[header.find(":")+1:header.find("_")+4]
+    features = features.strip()
+    features = re.sub('\s+',',', features)
+    features = features.split(",")
+    #features = features.split("  ")
+    
+    #features = ['MJD','FLT','FIELD','FLUXCAL','FLUXCALERR','SNR','MAG','MAGERR','SIM_MAG']
+    
+    data = [i.rstrip('\n') for i in data]
+    data = [i.lstrip('OBS:') for i in data]
+    data = [i.strip() for i in data]
+    data = [re.sub('\s+',',', i) for i in data]
+    data = [i.split(",") for i in data]
+    type_list =  len(data)*[typeSN]
+    type_list = [[i] for i in type_list]
+    
+    type_array = np.array(type_list)
+    data_array = np.array(data)
+    features = features + ['TYPE']
+    fulldata = np.concatenate((data_array,type_array), axis=1)
+    
+    df_data = pd.DataFrame(fulldata, columns = features)
 
-typeSN = sntype[sntype.find("=")+1:sntype.find(",")]
-typeSN = typeSN.strip()
-
-features = header[header.find(":")+1:header.find("_")+4]
-features = features.strip()
-features = re.sub('\s+',',', features)
-features = features.split(",")
-#features = features.split("  ")
-
-#features = ['MJD','FLT','FIELD','FLUXCAL','FLUXCALERR','SNR','MAG','MAGERR','SIM_MAG']
-
-data = [i.rstrip('\n') for i in data]
-data = [i.lstrip('OBS:') for i in data]
-data = [i.strip() for i in data]
-data = [re.sub('\s+',',', i) for i in data]
-data = [i.split(",") for i in data]
-type_list =  len(data)*[typeSN]
-type_list = [[i] for i in type_list]
-
-type_array = np.array(type_list)
-data_array = np.array(data)
-features = features + ['TYPE']
-fulldata = np.concatenate((data_array,type_array), axis=1)
-
-df_data = pd.DataFrame(fulldata, columns = features)
-
-print(files)
-
+print( '# of files in the folder: ', len(files))
 print ('job duration in s: ', datetime.now() - start)
